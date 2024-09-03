537fad5cefa0dee315896cd837dcc1e33c4860c5
Mohand <mohandbugrein@Mohands-MacBook-Pro.local>
2024-09-03 22:18:56 +0200

    changed the website to Google

--- wordpress/wp-content/plugins/wp-codespace/tests/example.spec.js
+++ wordpress/wp-content/plugins/wp-codespace/tests/example.spec.js
@@ -1,9 +1,9 @@
-// @ts-check
+// @ts-nocheck
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://google.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/google/);
 });
